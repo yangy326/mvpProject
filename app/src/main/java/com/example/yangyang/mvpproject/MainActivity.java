aad452ca328112ac7e2e4512ae3d6e3d5db1a9ad
yang yang <8707426552qq.comgit config --global user.name Your>
2019-07-30 00:56:21 +0800

    one

--- app/src/main/java/com/example/yangyang/mvpproject/MainActivity.java
+++ app/src/main/java/com/example/yangyang/mvpproject/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.yangyang.mvpproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.mryang.demo.R;
+
 public class MainActivity extends AppCompatActivity {
+
+    private int anInt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
