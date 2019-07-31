dde7166584c6b6f1647903bf48bcc330ba62dad1
yang yang <8707426552qq.comgit config --global user.name Your>
2019-08-01 00:11:36 +0800

    0011

--- app/src/main/java/com/example/yangyang/mvpproject/Main3Activity.java
+++ app/src/main/java/com/example/yangyang/mvpproject/Main3Activity.java
@@ -3,6 +3,8 @@ package com.example.yangyang.mvpproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.mryang.demo.R;
+
 public class Main3Activity extends AppCompatActivity {
 
     @Override
---
+++ app/src/main/java/com/example/yangyang/mvpproject/dfsdsas.java
@@ -0,0 +1,4 @@
+package com.example.yangyang.mvpproject;
+
+public class dfsdsas {
+}
